d1aecf53690b62459b36bc951a5f586714b7c223
yongchann <yongcgp@naver.com>
2025-02-11 23:48:06 +0900

    fix: Revert to production server API base URL

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-# API_BASE_URL = 'http://43.203.140.12:8080'
-API_BASE_URL = 'http://localhost:8080'
+API_BASE_URL = 'http://43.203.140.12:8080'
+# API_BASE_URL = 'http://localhost:8080'
 MONITOR_INTERVAL = 3  # 3초
